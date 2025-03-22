d7e2e8ef611696e2aeb9ae1b24f7c1279db183a6
zla247 <zla247@cs-vml-51.cmpt.sfu.ca>
2025-03-22 01:24:58 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="serial-neighbor",
-    version="0.1.0",
+    version="0.1.1",
     description="Serial order neighbor search module using spatial encoding.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
